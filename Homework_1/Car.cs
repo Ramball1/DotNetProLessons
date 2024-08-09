aa561615fc74befef17e3f308b48f631552d82f5
Ramball1 <ramball0981@gmail.com>
2024-08-09 19:23:43 +0200

    Update 1.1 (fix+)

---
+++ Homework_1/Car.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    public class Car
+    {
+        public string _brand = string.Empty;
+        public string _model = string.Empty;
+        public int _year = 0;
+        public double _mileage = 0d;
+
+        public Car(string brand, string model, int year, double mileage)
+        {
+            _brand = brand;
+            _model = model;
+            _year = year;
+            _mileage = mileage;
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Car Brand Name: {_brand}" +
+                $"\nCar Model Name: {_model}" +
+                $"\nCar Release Year: {_year}" +
+                $"\nCar Mileage: {_mileage}");
+        }
+
+        public double Drive(double distance)
+        {
+            const double minDistance = 0d;
+            const double maxDistance = 999999d;
+
+            if (distance < minDistance)
+            {
+                throw new ArgumentException("\nОшибка! Дистанция не может быть отрицательной! Попробуйте еще раз: ");
+            }
+
+            if (distance > maxDistance)
+            {
+                distance = maxDistance;
+                _mileage = distance;
+                return _mileage;
+            }
+
+            _mileage += distance;
+            return _mileage;
+        }
+
+        // Оба варианта работают первый вариант имеет свойство с логикой а второй только для чтения
+
+        //public int Age
+        //{
+        //    get
+        //    {
+        //        int currentYear = DateTime.Now.Year;
+        //        return currentYear - _year;
+        //    }
+        //}
+
+        public int Age => DateTime.Now.Year - _year;
+
+        public double Mileage
+        {
+            get { return _mileage; }
+            private set { _mileage = value; }
+        }
+
+        public double _averageSpeed = 0d;
+
+        public double Drive(double distance, double time)
+        {
+            const double minDistance = 0d;
+            const double minTime = 0d;
+
+            if (distance < minDistance || time < minTime)
+            {
+                throw new ArgumentException("\nОшибка! Дистанция или время не могут быть отрицательными! Попробуйте еще раз: ");
+            }
+
+            _averageSpeed = distance / time;
+            return _averageSpeed;
+        }
+
+        ~Car()
+        {
+            Console.WriteLine($"Машина уничтожена!");
+        }
+    }
+}
---
+++ Homework_1/ElectricCar.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    public class ElectricCar : Car
+    {
+        public double _batteryCapacity = 0;
+
+        public ElectricCar(string brand, string model, int year, double mileage, double batteryCapacity) : base(brand, model, year, mileage)
+        {
+            _batteryCapacity = batteryCapacity;
+        }
+
+        public void ShowElectricCarInfo()
+        {
+            Console.WriteLine($"Car Brand Name: {_brand}" +
+                $"\nCar Model Name: {_model}" +
+                $"\nCar Release Year: {_year}" +
+                $"\nCar Mileage: {_mileage}" +
+                $"\nCar Battery Capacity: {_batteryCapacity}");
+        }
+
+        public double Charge(double amount)
+        {
+            const double minBatteryCapacity = 0;
+            const double maxBatteryCapacity = 100;
+
+            if (amount < minBatteryCapacity)
+            {
+                throw new ArgumentException("\nОшибка! Количество для заряда не может быть отрицательным! Попробуйте еще раз: ");
+            }
+
+            _batteryCapacity += amount;
+
+            if (_batteryCapacity >= maxBatteryCapacity)
+            {
+                _batteryCapacity = maxBatteryCapacity;
+            }
+
+            return _batteryCapacity;
+        }
+
+        public double Charge(double initialBatteryCapacity, double distance, double consumptionPerKm)
+        {
+            double batteryLevel = initialBatteryCapacity;
+            double usedCharge = distance * consumptionPerKm;
+            double distanceAtFullCharge = batteryLevel / consumptionPerKm;
+
+            if (initialBatteryCapacity > 100 || initialBatteryCapacity < 0)
+            {
+                throw new ArgumentException("\nОшибка! Начальное количество заряда не может быть больше 100 % или меньше 0%! Попробуйте еще раз: ");
+            }
+
+            if (distance > distanceAtFullCharge)
+            {
+                double remainingDistance = distance - distanceAtFullCharge;
+
+                Console.WriteLine($"\nBattery depleted at {distanceAtFullCharge} km, Additional distance {remainingDistance} km");
+                return 0;
+            }
+
+            else
+            {
+                batteryLevel -= usedCharge;
+                return batteryLevel;
+            }
+        }
+    }
+}
